4bb911fe4d4c449c73b81b7f6b2826db79e12609
Stênia Felix <steniafelix@Stenias-MacBook-Pro.local>
2018-12-17 21:48:12 -0300

    Gonode-modulo-02

---
+++ src/database/migrations/20181207040747-create-users.js
@@ -0,0 +1,48 @@
+'use strict'
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        allowNull: true,
+        type: Sequelize.STRING
+      },
+      email: {
+        allowNull: true,
+        unique: true,
+        type: Sequelize.STRING
+      },
+      avatar: {
+        allowNull: true,
+        type: Sequelize.STRING
+      },
+      password_hash: {
+        allowNull: true,
+        type: Sequelize.STRING
+      },
+      provider: {
+        allowNull: true,
+        defaltValue: false,
+        type: Sequelize.BOOLEAN
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    })
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('users')
+  }
+}
